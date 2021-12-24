d4267d8f55681d39dc999c5dcc30d0ca778c27fe
Michael Dokuchaev <redisooon@ya.ru>
2021-12-24 19:34:42 +0700

    feat: create header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Header />
+      <Routes>
+          <Route path="shopping-cart" element={<div><p>123</p></div>}>
+          </Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
